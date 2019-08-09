30117d1fcf20f8c2ccb6897f1d1fa65b6d6f9151
Krops <andrekropes@yandex.ru>
2019-08-09 10:23:47 +0300

    Test reselect

--- src/components/Posts/PostsContainer.jsx
+++ src/components/Posts/PostsContainer.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 
 import PostsComponent from './PostsComponent.jsx';
 import {retrievePosts} from '../../store/Reducers/PostsReducers'
+import {createSelector} from 'reselect'
 
 class PostsContainer extends React.Component {
     constructor(props) {
@@ -40,6 +41,36 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => { 
     return bindActionCreators({retrievePosts: retrievePosts()}, dispatch)
   }
+
+
+
+  const makeGetPosts = () => createSelector(
+    (state) => state.posts.posts,
+    (state, props) => props.match.params.postId,
+    (posts, postId) => posts
+      .filter(id => posts[id].id === postId)
+      .map(id => {
+        return posts[id];
+      }),
+  );
+
+  const showAllPosts = (state) => state.posts.posts;
+
+  const postSelector = (posts, filter) => {
+    switch (filter) {
+      case 'SHOW_ALL':
+        return posts;
+      case 'SHOW_ONE':
+
+    }
+  }
+  
+  const mapState = () => {
+    const getPosts = makeGetPosts();
+    return (state, ownProps) => {
+      return {posts: getPosts(state, ownProps)};
+    };
+  };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
 
